064c657e286052353f8faecf4e11f1d2626276fb
Ibadnf <ibaddilahnf@gmail.com>
2020-10-26 12:13:43 +0700

    bentar pusing masih dipikirin

--- latihan09.py
+++ latihan09.py
@@ -23,3 +23,25 @@ personAndPets = [
 		]
 	}
 ]
+
+for x in personAndPets:
+	if x["name"] == "John Doe":
+		for p in x["pets"]:
+			print(p)
+
+
+class personPets:
+
+	def __init__(self, person=None, pets="Camel"):
+		self.person = person
+		self.pets = pets
+
+	def getPetbyPerson(self):
+		for x in personAndPets:
+			if x["name"] == self.person:
+				for p in x["pets"]:
+					print(p)
+
+
+pp = personPets(person="Luke Skywalker")
+pp.getPetbyPerson()

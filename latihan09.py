0407d8093a7f4a30326874f7186cc577d23ec565
Ibadnf <ibaddilahnf@gmail.com>
2020-10-26 11:08:29 +0700

    file kosong

---
+++ latihan09.py
@@ -0,0 +1,25 @@
+personAndPets = [
+	{
+		"name": "John Doe",
+		"pets": [
+			{"name": "Rooster"},
+			{"name": "Dog"}
+		]
+	},
+
+	{
+		"name": "Luke Skywalker",
+		"pets": [
+			{"name": "Duck"},
+			{"name": "Camel"}
+		]
+	},
+
+	{
+		"name": "Padme Amidala",
+		"pets": [
+			{"name": "Bird"},
+			{"name": "Fish"}
+		]
+	}
+]
